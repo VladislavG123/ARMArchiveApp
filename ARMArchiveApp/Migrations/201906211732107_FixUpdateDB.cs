32a92d11930f5bcf612f6aa3bbcae8fa892ef0e1
Владислав Горохов <gbg.0101@mail.ru>
2019-06-21 23:33:16 +0600

    Исправлена ошибка с миграцией (2)

---
+++ ARMArchiveApp/Migrations/201906211732107_FixUpdateDB.cs
@@ -0,0 +1,16 @@
+namespace ARMArchiveApp.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class FixUpdateDB : DbMigration
+    {
+        public override void Up()
+        {
+        }
+        
+        public override void Down()
+        {
+        }
+    }
+}
